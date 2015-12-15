77702a3c1468b17aca621e43abf4ec4d5d4064a1
Fabricio Flores <fabrifloresg@gmail.com>
2015-12-15 11:03:03 -0500

    Read the endpoint from local

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -9,7 +9,13 @@ searchOnNiara = function (word) {
   var query = word.selectionText;
   console.log(query);
   //we need to send the query as a param to an exposed url
-  chrome.tabs.create({ url: 'http://localhost:9000' });
+  var endpointUrl;
+  chrome.storage.sync.get(['niaraUrl'], function (items) {
+    if (items.niaraUrl) {
+      endpointUrl = items.niaraUrl;
+    }
+  });
+  chrome.tabs.create({ url: endpointUrl });
 };
 chrome.contextMenus.create({
   title: 'Search "%s" in Niara',
