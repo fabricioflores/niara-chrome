83c4a4da9f453d4c11b424311e374123f086da57
Fabricio Flores <fabrifloresg@gmail.com>
2015-12-15 12:45:38 -0500

    Build url for Niara endpoint

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -5,21 +5,37 @@ chrome.runtime.onInstalled.addListener(details => {
 });
 
 var searchOnNiara;
+var buildUrl;
+
+buildUrl = (url, query) => {
+  var buildedUrl = '';
+
+  if (url.lastIndexOf('/') !== (url.length-1) ) {
+    buildedUrl = buildedUrl + '/';
+  }
+
+  if (url.indexOf('http') < 0) {
+    buildedUrl = 'https://' + url;
+  }
+  
+  buildedUrl = buildedUrl + '/#/integration/source:chrome-extension+entity:' + query;
+  return buildedUrl;
+};
+
 searchOnNiara = word => {
   var query = word.selectionText;
-  console.log(query);
-  //we need to send the query as a param to an exposed url
   var endpointUrl;
+
   chrome.storage.sync.get([
     'niaraUrl'
   ], function(items) {
     if (items.niaraUrl) {
-      endpointUrl = items.niaraUrl;
+      endpointUrl = buildUrl(items.niaraUrl, query);
+      chrome.tabs.create({url: endpointUrl});
     }else{
       chrome.runtime.openOptionsPage();
     }
   });
-  chrome.tabs.create({url: endpointUrl});
 };
   chrome.contextMenus.create({
     title: 'Search "%s" in Niara',
--- app/scripts/background.js
+++ app/scripts/background.js
@@ -5,19 +5,35 @@ chrome.runtime.onInstalled.addListener(function (details) {
 });
 
 var searchOnNiara;
+var buildUrl;
+
+buildUrl = function (url, query) {
+  var buildedUrl = '';
+
+  if (url.lastIndexOf('/') !== url.length - 1) {
+    buildedUrl = buildedUrl + '/';
+  }
+
+  if (url.indexOf('http') < 0) {
+    buildedUrl = 'https://' + url;
+  }
+
+  buildedUrl = buildedUrl + '/#/integration/source:chrome-extension+entity:' + query;
+  return buildedUrl;
+};
+
 searchOnNiara = function (word) {
   var query = word.selectionText;
-  console.log(query);
-  //we need to send the query as a param to an exposed url
   var endpointUrl;
+
   chrome.storage.sync.get(['niaraUrl'], function (items) {
     if (items.niaraUrl) {
-      endpointUrl = items.niaraUrl;
+      endpointUrl = buildUrl(items.niaraUrl, query);
+      chrome.tabs.create({ url: endpointUrl });
     } else {
       chrome.runtime.openOptionsPage();
     }
   });
-  chrome.tabs.create({ url: endpointUrl });
 };
 chrome.contextMenus.create({
   title: 'Search "%s" in Niara',
